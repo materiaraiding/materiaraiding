7220528520de5e8808f47837047389ff3a7cbf65
Poddy13 <gnazare2003@gmail.com>
2025-06-04 17:11:55 +1000

    FieldOps icon integrated

--- .vitepress/theme/components/lists/difficultyTypes.js
+++ .vitepress/theme/components/lists/difficultyTypes.js
@@ -5,5 +5,5 @@ export const difficultyTypes = [
 	{ type: 'Ultimate', icon: '/images/icons/highendduty.webp', colorClass: 'ultimate-color' },
 	{ type: 'Criterion', icon: '/images/icons/variant.webp', colorClass: 'criterion-color' },
 	{ type: 'Unreal', icon: '/images/icons/trial.webp', colorClass: 'unreal-color' },
-	{ type: 'Field Operations', icon: '/images/icons/raid.webp', colorClass: 'fieldops-color' },
+	{ type: 'Field Operations', icon: '/images/icons/fieldops.webp', colorClass: 'fieldops-color' },
 ];
