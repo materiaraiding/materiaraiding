9ab2a917d8343bdd1fd1d0a52c5d6cf0129d606d
Melvin <melvinhkusuma@gmail.com>
2025-06-22 17:22:25 +1000

    fix?(useScrollDirection): potential fix for Safari mobile nav hiding behaviour

--- .vitepress/theme/hooks/useScrollDirection.ts
+++ .vitepress/theme/hooks/useScrollDirection.ts
@@ -2,24 +2,33 @@
  * Listens to the user's scroll direction, 
  * accepts a callback with a boolean of a downward scroll.
  * 
+ * Options:
  * @param isMobile - Listen only during mobile widths (960px). False by default.
+ * @param pageTopTreshold - Sets a threshold scroll distance past the top of the page in order for the callback to occur. Zero by default.
  */
 export default function useScrollDirection(
   onDirectionChange: (isDown: boolean) => void,
-  { isMobile = false }: { isMobile: boolean }
+  options?: {
+    isMobile?: boolean,
+    pageTopThreshold?: number
+  }
 ): void {
+  const isMobile = options?.isMobile ?? false;
+  const pageTopThreshold = options?.pageTopThreshold ?? 0;
+
   let lastScrollY = 0;
   let scrolledDownFlag = false;
 
   const scrollListener = () => {
-    const isScrollingDown = window.scrollY > lastScrollY;
+    const newY = window.scrollY;
+    const isScrollingDown = newY > lastScrollY && newY > pageTopThreshold;
 
     if (isScrollingDown !== scrolledDownFlag) {
       onDirectionChange(isScrollingDown);
       scrolledDownFlag = isScrollingDown;
     }
 
-    lastScrollY = window.scrollY;
+    lastScrollY = newY;
   }
 
   const updateScrollListener = () => {
