c43e23261b871bf4f85ae1cdfe8a61ba7471ba1a
Melvin <melvinhkusuma@gmail.com>
2025-06-26 16:31:33 +1000

    re ordered fieldops/criterion for guideHome

--- .vitepress/theme/components/lists/difficultyTypes.ts
+++ .vitepress/theme/components/lists/difficultyTypes.ts
@@ -4,6 +4,6 @@ export const difficultyTypes = [
 	{ type: 'Extreme', icon: '/images/icons/trial.webp', colorClass: 'extreme-color', order: 1 },
 	{ type: 'Savage', icon: '/images/icons/raid.webp', colorClass: 'savage-color', order: 2 },
 	{ type: 'Ultimate', icon: '/images/icons/highendduty.webp', colorClass: 'ultimate-color', order: 3 },
-	{ type: 'Criterion', icon: '/images/icons/variant.webp', colorClass: 'criterion-color', order: 4 },
 	{ type: 'Field Operations', icon: '/images/icons/fieldops.webp', colorClass: 'fieldops-color', urlOverride: 'fieldops', order: 7 },
+	{ type: 'Criterion', icon: '/images/icons/variant.webp', colorClass: 'criterion-color', order: 4 },
 ];
