25ae75ccc90afe99a5ab8fe70575b5873319f0a7
Em <marcusingram@outlook.com.au>
2025-06-27 10:44:16 +1000

    Unreal Icon

--- .vitepress/theme/components/lists/difficultyTypes.ts
+++ .vitepress/theme/components/lists/difficultyTypes.ts
@@ -1,6 +1,6 @@
 export const difficultyTypes = [
 	{ type: 'Chaotic', icon: '/images/icons/chaotic.webp', colorClass: 'chaotic-color', order: 5 },
-	{ type: 'Unreal', icon: '/images/icons/trial.webp', colorClass: 'unreal-color', order: 6 },
+	{ type: 'Unreal', icon: '/images/icons/unreal.webp', colorClass: 'unreal-color', order: 6 },
 	{ type: 'Extreme', icon: '/images/icons/trial.webp', colorClass: 'extreme-color', order: 1 },
 	{ type: 'Savage', icon: '/images/icons/raid.webp', colorClass: 'savage-color', order: 2 },
 	{ type: 'Ultimate', icon: '/images/icons/highendduty.webp', colorClass: 'ultimate-color', order: 3 },
