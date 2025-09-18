1422b1250c20aba6f02cea701414060ca8ca5b81
Melvin <melvinhkusuma@gmail.com>
2025-09-18 23:03:58 +1000

    feat(router loading bar): added a time threshold in order for the loading bar to display.

--- .vitepress/theme/hooks/useRouterLoadingBar.ts
+++ .vitepress/theme/hooks/useRouterLoadingBar.ts
@@ -1,17 +1,17 @@
 export default function useRouterLoadingBar() {
   function loading() {
     const loadingBar = document.getElementById("loading-bar");
-    if (!loadingBar) { return; }
+    if (!loadingBar) return;
     loadingBar.classList.remove("finished");
     loadingBar.classList.add("loading");
   }
 
   function finished() {
     const loadingBar = document.getElementById("loading-bar");
-    if (!loadingBar) { return; }
+    if (!loadingBar) return;
     loadingBar.classList.replace("loading", "finished");
-    setTimeout(() => { loadingBar.classList.remove("finished") }, 300);
+    setTimeout(() => loadingBar.classList.remove("finished"), 300);
   }
 
-  return { loading, finished }
+  return { loading, finished };
 }
